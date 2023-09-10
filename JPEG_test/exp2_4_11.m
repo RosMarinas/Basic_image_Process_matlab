73151ed547ab899570f73393c0a693b622cec36c
swh <swh031019@gmail.com>
2023-09-10 19:10:38 +0800

    version2.0

--- JPEG_test/exp2_4_11.m
+++ JPEG_test/exp2_4_11.m
@@ -1,8 +1,11 @@
 %解码
+
 close all;
 clear;
 clc;
 global huffman_table;
+load("hall.mat");
+imshow(hall_gray);
 load('jpegcodes.mat');
 load('JpegCoeff.mat');
 huffman_table={
@@ -46,48 +49,69 @@ end
 EOB=[1,0,1,0];
 ZRL=[1,1,1,1,1,1,1,1,0,0,1];
 RunSize=[0,0];
-encode=[];
-accode=[];
-ac_huff_code=[];
+encode=[];%待解码，需要从AC_code中取出
+accode=[];%解出的ac码
+ac_huff_code=[];%ACTAB中取出的huffman编码
 i=1;
-while i<length(AC_code)
+count=0;
+while i<=length(AC_code)
     encode=[encode AC_code(i)];
     if length(encode)==4
             if encode==EOB
-                RunSize=[0,0];
-                accode=
+                %RunSize=[0,0];
+                accode=[accode,zeros(1,63-(length(accode)-63*count))];
+                %结束并更新
+                count=count+1;
+                i=i+1;
+                encode=[];
+                continue;
             end
     end
     if length(encode)==11
             if encode==ZRL
-                RunSize=[16,0];
+                %RunSize=[16,0];
+                accode=[accode,zeros(1,16)];
+                %结束并更新
+                i=i+1;
+                encode=[];
+                continue;  
             end
     end
+    %先找到对应的码长，再逐个比较，从而减少比较次数
     [temp,~,~]=find(ACTAB(:,3)==length(encode));
-    for j=1:length(x)
-        ac_huff_code=ACTAB(temp(i),4:length(encode)+3);
+    for j=1:length(temp)
+        ac_huff_code=ACTAB(temp(j),4:length(encode)+3);
         if isequal(ac_huff_code,encode)
-            RunSize=[ACTAB(temp(i),1),ACTAB(temp(i),2)];
-            break;
+            RunSize=[ACTAB(temp(j),1),ACTAB(temp(j),2)];
+            accode=[accode,zeros(1,RunSize(1))];
+            accode=[accode,icomplement(num2str(AC_code(i+1:i+RunSize(2))))];
+            %结束并更新
+            encode=[];
+            i=i+RunSize(2);
+            continue;
         end
     end
-
-    
+    i=i+1;
 end
-7
+h=icomplement(num2str(h));
+w=icomplement(num2str(w));
+accode=reshape(accode,[63,h*w/64]);
+hall_quant_izigzag(1,:)=DC_quant;
+hall_quant_izigzag(2:64,:)=accode;
+%逆zigzag
 
-
-
-
-
-
-
-      
-         
-
-
-
-
+hall_quant=[h,w];
+m=1;
+for r=1:8:h
+    for s=1:8:w
+        hall_quant(r:r+7,s:s+7)=izig_zag(hall_quant_izigzag(:,m));
+        m=m+1;
+        hall_dct(r:r+7,s:s+7)=hall_quant(r:r+7,s:s+7).*QTAB;
+        hall(r:r+7,s:s+7)=double(idct2(hall_dct(r:r+7,s:s+7)))+ones(8)*128;
+    end
+end
+figure;
+imshow(uint8(hall));
 
 
 % huffman函数(没有用上)
@@ -105,5 +129,3 @@ end
 %         end
 %     end
 % end
-        
-%
